6e2d05535254ca51cd32496354075173a4c53651
smedur <smeduri@sapient.com>
2018-04-03 18:26:38 +0530

    [feature] Function 2 is added.

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
 
+	private static void function_2() {
+		System.out.println("Function 2 is added.");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello world - Github. Welcome!");
 	}
